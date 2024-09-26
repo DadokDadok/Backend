a4c6e02032d8b58a949f0ca49291554b108ac6a3
onebbu <vmfdo23@gmail.com>
2024-09-26 16:59:23 +0900

    UPDATE: DB 테스트 데이터 추가

---
+++ pl_Backend/src/main/java/com/dadokdadok/myapp/dadoktest/InputVO.java
@@ -0,0 +1,14 @@
+package com.dadokdadok.myapp.dadoktest;
+
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class InputVO {
+    private String input;
+    private String output;
+}
--- pl_Backend/src/main/java/com/dadokdadok/myapp/dadoktest/TestController.java
+++ pl_Backend/src/main/java/com/dadokdadok/myapp/dadoktest/TestController.java
@@ -3,6 +3,7 @@ package com.dadokdadok.myapp.dadoktest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -11,14 +12,37 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TestController {
 
+    @Autowired
+    private InputDTO inputDTO;
+
     @GetMapping("/test/{testId}")
     public String testGet() {
         return "Get 성공: pipe-line-test";
     }
 
     @PostMapping("/test/submit")
-    public String testPost(@RequestBody InputDTO inputDTO) {
-        log.info("입력값: ", inputDTO.getInput());
-        return "Post 성공: pipe-line-test / 인풋값: " + inputDTO.getInput();
+    public String testPost(@RequestBody InputVO inputVO) {
+        log.info("입력값: ", inputVO.getInput());
+        return "Post 성공: pipe-line-test / 인풋값: " + inputVO.getInput();
+    }
+
+    @GetMapping("get/{output}")
+    public String getInput(@PathVariable String output) {
+        return inputDTO.findInput(output);
+    }
+
+    @PostMapping("post/{output}")
+    public void createInput(@PathVariable String output) {
+        inputDTO.addInput(output);
+    }
+
+    @PutMapping("put/{output}")
+    public void updateInput(@PathVariable String output) {
+        inputDTO.updateInput(output);
+    }
+
+    @DeleteMapping("delete/{output}")
+    public void deleteInput(@PathVariable String output) {
+        inputDTO.deleteInput(output);
     }
 }
